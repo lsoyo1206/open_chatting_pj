cbaab0ac689ca9dc203cbe525e8c7af2dd9cb7d0
yeswon9874 <yeswon9874@gmail.com>
2023-03-21 20:39:57 +0900

    인덱스 controller 세팅

---
+++ src/test/java/com/subpj/openchatting/OpenchattingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.subpj.openchatting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenchattingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
